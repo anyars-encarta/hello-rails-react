f1f294ad11890c571350c88f001126843f55210c
anyars-encarta <anyarsencarta@gmail.com>
2024-01-15 08:06:36 +0000

    Remove project files

--- app/javascript/application.js
+++
@@ -1,8 +0,0 @@
-// Entry point for the build script in your package.json
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Message from '../javascript/components/Message';
-
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<Message />, document.getElementById('root'));
-});
