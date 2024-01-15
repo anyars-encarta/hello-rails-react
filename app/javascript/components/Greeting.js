07f590ddac990648ae37d04d8351e6f27fa002be
anyars-encarta <anyarsencarta@gmail.com>
2024-01-15 23:19:28 +0000

    Add Timer to changing greetings

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -5,6 +5,7 @@ import '../../assets/stylesheets/greeting.css';
 
 const Greeting = ({ randomMessage, fetchRandomMessage }) => {
   const [isLoading, setIsLoading] = useState(true);
+  const [countdown, setCountdown] = useState(10);
 
   useEffect(() => {
      const fetchMessage = async () => {
@@ -19,6 +20,20 @@ const Greeting = ({ randomMessage, fetchRandomMessage }) => {
     fetchMessage();
   }, [fetchRandomMessage]);
 
+  useEffect(() => {
+    if (countdown === 0) {
+      // Fetch new random message and reset countdown
+      fetchRandomMessage();
+      setCountdown(10);
+    } else {
+      // Decrease countdown every second
+      const timer = setTimeout(() => setCountdown(countdown - 1), 1000);
+
+      // Clear the timer when component unmounts
+      return () => clearTimeout(timer);
+    }
+  }, [countdown, fetchRandomMessage]);
+
   if (isLoading) {
     return <div class="loading">Loading...</div>;
   }
@@ -27,7 +42,7 @@ const Greeting = ({ randomMessage, fetchRandomMessage }) => {
        <div class="greeting-content">
          <h1>Greeting of the day</h1>
          <p>"{randomMessage}"</p>
-         <h2>Greeting changes in: 0</h2>
+         <h2>Greeting changes in: {countdown}</h2>
        </div>
      );
    };
